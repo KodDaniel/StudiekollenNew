8043260321ae87d2bfcc9460d3fd08f7dbd25606
KodDaniel <daniel.ahlin1@gmail.com>
2018-08-28 13:50:21 +0200

    x

--- StudiekollenNew/Controllers/HomeController.cs
+++ StudiekollenNew/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace StudiekollenNew.Controllers
 {
+    // AllowAnonumous tillåter vem som helt access, i det här fallet till alla Actions i HomeController.
     [AllowAnonymous]
     public class HomeController : Controller
     {
