cfd09270c807bd28473e0c7ac19ac1026514ae45
KodDaniel <daniel.ahlin1@gmail.com>
2018-09-11 14:31:22 +0200

    d

--- StudiekollenNew/App_Start/WebApiConfig.cs
+++ StudiekollenNew/App_Start/WebApiConfig.cs
@@ -9,15 +9,15 @@ namespace StudiekollenNew
 {
     public static class WebApiConfig
     {
-        public static void Register(HttpConfiguration config)
-        {
-            config.MapHttpAttributeRoutes();
+        //public static void Register(HttpConfiguration config)
+        //{
+        //    config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
+        //    config.Routes.MapHttpRoute(
+        //        name: "DefaultApi",
+        //        routeTemplate: "api/{controller}/{id}",
+        //        defaults: new { id = RouteParameter.Optional }
+        //    );
+        //}
     }
 }
